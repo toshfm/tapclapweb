5dd596dad1fc09aecd3f8571a9b80d538fc4249f
AntonBitsura <tel-fm@ya.ru>
2026-02-16 12:14:01 +0300

    Refactor

---
+++ src/settings.1680c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Logo.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",main:"6211a"}};
