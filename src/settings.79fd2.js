92a2fe6d0da57a9250a71eaa0e50beed81658bd1
Anton Bitsura <tel-fm@ya.ru>
2026-02-23 22:52:39 +0300

    add rainbow

--- src/settings.79fd2.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Logo.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",main:"f0a81"}};
